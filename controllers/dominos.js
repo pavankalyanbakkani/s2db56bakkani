35e3b7a1ff9318e8176890c3aa62de48383a311c
pavankalyanbakkani <98134544+pavankalyanbakkani@users.noreply.github.com>
2022-04-19 20:19:03 -0500

    Update dominos.js

--- controllers/dominos.js
+++ controllers/dominos.js
@@ -48,9 +48,18 @@ exports.dominos_create_post = async function(req, res) {
   
  
 // Handle dominos delete form on DELETE. 
-exports.dominos_delete = function(req, res) { 
-    res.send('NOT IMPLEMENTED: dominos delete DELETE ' + req.params.id); 
+exports.dominos_delete = async function(req, res) { 
+    console.log("delete "  + req.params.id) 
+    try { 
+        result = await dominos.findByIdAndDelete( req.params.id) 
+        console.log("Removed " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": Error deleting ${err}}`); 
+    } 
 }; 
+   
  
 // Handle dominos update form on PUT. 
 exports.dominos_update_put = async function(req, res) {

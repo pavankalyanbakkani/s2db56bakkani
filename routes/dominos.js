4ced1758afb21a33168e8a093f807e14de930a46
pavankalyanbakkani <98134544+pavankalyanbakkani@users.noreply.github.com>
2022-04-19 22:43:38 -0500

    Update dominos.js

--- routes/dominos.js
+++ routes/dominos.js
@@ -7,5 +7,5 @@ router.get('/', dominos_controlers.dominos_view_all_Page );
 
 router.get('/detail', dominos_controlers.dominos_view_one_Page);
 router.get('/create', dominos_controlers.dominos_create_Page);
-
+router.get('/update', dominos_controlers.dominos_update_Page);
 module.exports = router;

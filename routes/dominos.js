c6dfe4564b728f3373e7ae6e7b2eb6c75f5e0875
pavankalyanbakkani <98134544+pavankalyanbakkani@users.noreply.github.com>
2022-04-19 21:50:33 -0500

    Update dominos.js

--- routes/dominos.js
+++ routes/dominos.js
@@ -5,9 +5,5 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', dominos_controlers.dominos_view_all_Page );
 
-router.get('/detail', dominos_controlers.dominos_view_one_Page);
-router.get('/create', dominos_controlers.dominos_create_Page); 
-router.get('/update', dominos_controlers.dominos_update_Page);
-router.get('/delete', dominos_controlers.dominos_delete_Page); 
 
 module.exports = router;

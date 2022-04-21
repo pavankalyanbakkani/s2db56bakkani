720a597f7a7aab5b982c606b38b84ca463afdf51
pavankalyanbakkani <98134544+pavankalyanbakkani@users.noreply.github.com>
2022-04-21 15:10:54 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy( 
+  function(username, password, done) { 
+    Account.findOne({ username: username }, function (err, user) { 
+      if (err) { return done(err); } 
+      if (!user) { 
+        return done(null, false, { message: 'Incorrect username.' }); 
+      } 
+      if (!user.validPassword(password)) { 
+        return done(null, false, { message: 'Incorrect password.' }); 
+      } 
+      return done(null, user); 
+    }); 
+  }));
 
 const connectionString =  process.env.MONGO_CON 
 mongoose = require('mongoose'); 
@@ -34,6 +50,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({ 
+  secret: 'keyboard cat', 
+  resave: false, 
+  saveUninitialized: false 
+})); 
+app.use(passport.initialize()); 
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -42,6 +66,41 @@ app.use('/dominos', dominosRouter);
 app.use('/addmods', addmodsRouter);
 app.use('/selector', selectorRouter);
 app.use('/resource', resourceRouter);
+
+
+// passport config 
+// Use the existing connection 
+// The Account model  
+var Account =require('./models/account'); 
+ 
+passport.use(new LocalStrategy(Account.authenticate())); 
+passport.serializeUser(Account.serializeUser()); 
+passport.deserializeUser(Account.deserializeUser());
+
+// We can seed the collection if needed on server start
+async function recreateDB(){ 
+  // Delete everything 
+ await dominos.deleteMany(); 
+
+ let instance1 = new dominos({Itemname:"cheese burst", Quantity:2, price:"two five USD"}); 
+ instance1.save( function(err,doc) { 
+ if(err) return console.error(err); 
+ console.log("First object saved") 
+ });
+
+ let instance2 = new dominos({Itemname:"farm house", Quantity:3, price:"two five USD"}); 
+ instance2.save( function(err,doc) { 
+ if(err) return console.error(err); 
+ console.log("Second object saved") 
+ });
+
+ let instance3 = new dominos({Itemname:"chicken toast", Quantity:4, price:"two five USD"}); 
+ instance3.save( function(err,doc) { 
+ if(err) return console.error(err); 
+ console.log("Third object saved") 
+ });
+
+}
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));

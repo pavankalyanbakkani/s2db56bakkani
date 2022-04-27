3c34f12f82e561b086ed4688da19dff71ee855bc
pavankalyanbakkani <98134544+pavankalyanbakkani@users.noreply.github.com>
2022-04-27 17:20:43 -0500

    Update dominos.js

--- routes/dominos.js
+++ routes/dominos.js
@@ -2,11 +2,23 @@ var express = require('express');
 const dominos_controlers= require('../controllers/dominos');
 var router = express.Router();
 
+
+// A little function to check if we have an authorized user and continue on
+or
+// redirect to login.
+const secured = (req, res, next) => {
+if (req.user){
+return next();
+}
+req.session.returnTo = req.originalUrl;
+res.redirect("/login");
+}
+
 /* GET home page. */
 router.get('/', dominos_controlers.dominos_view_all_Page );
 
 router.get('/detail', dominos_controlers.dominos_view_one_Page);
 router.get('/create', dominos_controlers.dominos_create_Page);
-router.get('/update', dominos_controlers.dominos_update_Page);
+router.get('/update',secured, dominos_controlers.dominos_update_Page);
 router.get('/delete', dominos_controlers.dominos_delete_Page);
 module.exports = router;
